c4930f5afd39cb8e42d7e744f30db258b10ffd76
Yanter44 <User@WIN-0H64IFHIJH9>
2025-08-22 19:41:04 +0300

    TestGitHubActions2

--- MpParserAPI/Program.cs
+++ MpParserAPI/Program.cs
@@ -43,32 +43,32 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<ParsersHostedService>();
 
-//builder.WebHost.UseKestrel(options =>
-//{
-//    options.Listen(System.Net.IPAddress.Any, 9090);
-//});
+builder.WebHost.UseKestrel(options =>
+{
+    options.Listen(System.Net.IPAddress.Any, 9090);
+});
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowFrontend", policy =>
-//    {
-//        policy
-//            .WithOrigins("https://resortlehi.ru")
-//            .AllowCredentials()
-//            .AllowAnyHeader()
-//            .AllowAnyMethod();
-//    });
-//});
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAllOrigins", policy =>
+    options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:8000")
-          .AllowCredentials()
-          .AllowAnyHeader()
-          .AllowAnyMethod();
+        policy
+            .WithOrigins("https://resortlehi.ru")
+            .AllowCredentials()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("AllowAllOrigins", policy =>
+//    {
+//        policy.WithOrigins("http://localhost:8000")
+//          .AllowCredentials()
+//          .AllowAnyHeader()
+//          .AllowAnyMethod();
+//    });
+//});
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
@@ -121,8 +121,8 @@ app.MapHub<ParserHub>("/parserHub");
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-//app.UseCors("AllowFrontend"); 
-app.UseCors("AllowAllOrigins");
+app.UseCors("AllowFrontend"); 
+//app.UseCors("AllowAllOrigins");
 app.MapControllers(); 
 
 app.Run();
