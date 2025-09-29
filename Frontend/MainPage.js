3feca6633561879ec4135b894f42d7340a84b531
Yanter44 <User@WIN-0H64IFHIJH9>
2025-09-29 22:28:04 +0300

    так ну сейчас вроде точно все вернул

--- Frontend/MainPage.js
+++ Frontend/MainPage.js
@@ -1,9 +1,29 @@
-
+const tg = window.Telegram.WebApp;
 var primarybutton = document.getElementsByClassName('primaryButton')[0];
 var secondaryButton = document.getElementsByClassName('secondaryButton')[0];
 
 console.log(config.API_BASE); 
 console.log(config.DefaultStartFileLocation); 
+
+document.addEventListener("DOMContentLoaded", () => {
+    function initTelegramWebApp() {
+        if (window.Telegram && window.Telegram.WebApp) {
+            const tg = window.Telegram.WebApp;
+
+            console.log('WebApp version:', tg.version);
+            tg.ready();
+
+            if (tg.BackButton) {          
+                tg.BackButton.hide();            
+            }
+
+        } else {
+            setTimeout(initTelegramWebApp, 100);
+        }
+    }
+
+    initTelegramWebApp();
+});
 
 primarybutton.addEventListener('click', () => {
   window.location.href = `${config.DefaultStartFileLocation}/CreateAdminPanelPage/CreateAdminPanelPage.html`;
@@ -30,4 +50,3 @@ secondaryButton.addEventListener('click', async () => {
     window.location.href = `${config.DefaultStartFileLocation}/EnterToExistParserAccount/EnterToExistParserAccount.html`;
   }
 });
-
